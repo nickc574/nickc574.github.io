e764221220121b6062563a36928bd5e3be38e6f5
nick <nickc574@gmail.com>
2023-11-12 16:21:33 -0800

    react learning

---
+++ react-ts-bs-app/src/components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import { Fragment } from "react"
+
+function ListGroup() {
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    items = [];
+
+    if (items.length === 0)
+        return (
+            <>
+                <h1>List</h1>
+                <p>No item Found</p>
+            </>
+
+        );
+
+    return (
+        <>
+            <h1>List</h1>
+
+            { items.length == 0 ? <p>No item Found</p> : null }
+            
+            <ul className="list-group">
+                {items.map(item => <li key={item}>{item}</li>)}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
